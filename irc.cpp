869c2259500353cc2677628a892f059fe105e56c
sehe <sgheeren@gmail.com>
2016-01-27 21:18:56 +0100

    Use shared_ptr

--- irc.cpp
+++ irc.cpp
@@ -1,60 +1,59 @@
 #include "irc.hpp"
 #include <iostream>
-#include <thread>
-#include <chrono>
 #include <boost/bind.hpp>
-#include <boost/tokenizer.hpp>
+#include <boost/make_shared.hpp>
 
 Irc::Irc(const std::string &server, const std::string &port, const std::function<void()> onConnect)
-	: _server(server), _port(port), _onConnect(onConnect), _socket(_ios)
+    : _server(server), _port(port), _onConnect(onConnect), 
+      _socket(boost::make_shared<boost::asio::ip::tcp::socket>(boost::ref(_ios)))
 {
-	// Ping back handler
-	_readHandlers.push_back([this](const boost::tokenizer<boost::char_separator<char> > &tokenizer) {
-		std::vector<std::string> tokens(begin(tokenizer), end(tokenizer)); 
+    // Ping back handler
+    _readHandlers.push_back([this](const boost::tokenizer<boost::char_separator<char> > &tokenizer) {
+        std::vector<std::string> tokens(begin(tokenizer), end(tokenizer)); 
 
-		if(tokens[0].compare("PING") == 0)
-			_pong(tokens[1]);	
-	});
+        if(tokens[0].compare("PING") == 0)
+            _pong(tokens[1]);   
+    });
 
 }
 
 void Irc::connect()
 {
-	boost::asio::ip::tcp::resolver resolver(_ios);
-	boost::asio::ip::tcp::resolver::query query(_server, _port);
-	boost::asio::ip::tcp::resolver::iterator it = resolver.resolve(query);
-	boost::asio::ip::tcp::resolver::iterator end;
-	boost::system::error_code error = boost::asio::error::host_not_found;
+    boost::asio::ip::tcp::resolver resolver(_ios);
+    boost::asio::ip::tcp::resolver::query query(_server, _port);
+    boost::asio::ip::tcp::resolver::iterator it = resolver.resolve(query);
+    boost::asio::ip::tcp::resolver::iterator end;
+    boost::system::error_code error = boost::asio::error::host_not_found;
 
-	while(it != end)
-	{
-		if(!error)
-			break;
+    while(it != end)
+    {
+        if(!error)
+            break;
 
-		std::cout << "Connecting to " << _server << " " << _port << std::endl;
+        std::cout << "Connecting to " << _server << " " << _port << std::endl;
 
-		boost::asio::async_connect(_socket, it,
-        	boost::bind(&Irc::_connectHandler, this, error)
-		);
+        boost::asio::async_connect(*_socket, it,
+            boost::bind(&Irc::_connectHandler, this, error)
+        );
 
-		it++;
-	
-		if(error)
-			std::cout << "Error : " << error.message() << std::endl;
+        it++;
 
-	}
+        if(error)
+            std::cout << "Error : " << error.message() << std::endl;
 
-	if(error)
-		std::cout << "Error connectinf to " << _server << " " << error.message() << std::endl;
-	else
-		std::cout << "Connection success" << std::endl;
-	
+    }
+
+    if(error)
+        std::cout << "Error connectinf to " << _server << " " << error.message() << std::endl;
+    else
+        std::cout << "Connection success" << std::endl;
+
 }
 
 void Irc::close()
 {
-	_socket.close();
-	_ios.stop();
+    _socket->close();
+    _ios.stop();
 }
 
 void Irc::user(const std::string &username)
@@ -103,13 +102,13 @@ void Irc::command(const std::string &cmd, const std::string &to, const std::stri
 
 void Irc::run()
 {
-	boost::asio::async_read_until(_socket, _buffer, "\r\n",
-		boost::bind(&Irc::_read, this,
-			boost::asio::placeholders::error
-		)
-	);
+    boost::asio::async_read_until(*_socket, _buffer, "\r\n",
+        boost::bind(&Irc::_read, this,
+            boost::asio::placeholders::error
+        )
+    );
 
-	_ios.run();
+    _ios.run();
 }
 
 /*
@@ -118,46 +117,45 @@ void Irc::run()
 
 void Irc::_read(const boost::system::error_code &error)
 {
-	if(error)
-	{
-		std::cerr << error.message() << std::endl;
-		close();
-	}
-	else
-	{
-		std::string data(buffers_begin(_buffer.data()), buffers_begin(_buffer.data()) + _buffer.size());
-		std::cout << data << std::endl;		
+    if(error)
+    {
+        std::cerr << "Error in read : " << error.message() << std::endl;
+    }
+    else
+    {
+        std::string data(buffers_begin(_buffer.data()), buffers_begin(_buffer.data()) + _buffer.size());
+        std::cout << data << std::endl;     
 
-		boost::char_separator<char> sep("!@:; ");
-    	boost::tokenizer<boost::char_separator<char> > tokenizer(data, sep);
+        boost::char_separator<char> sep("!@:; ");
+        boost::tokenizer<boost::char_separator<char> > tokenizer(data, sep);
 
-		_readHandler(tokenizer);
-		boost::asio::async_read_until(_socket, _buffer, "\r\n",
-			boost::bind(&Irc::_read, this,
-				boost::asio::placeholders::error
-			)
-		);
-		
-	}
+        _readHandler(tokenizer);
+        boost::asio::async_read_until(*_socket, _buffer, "\r\n",
+            boost::bind(&Irc::_read, this,
+                boost::asio::placeholders::error
+            )
+        );
+
+    }
 }
 
 inline void Irc::_send(std::string &message)
 {
-	boost::asio::write(_socket, boost::asio::buffer(message + "\r\n"));
+    boost::asio::write(*_socket, boost::asio::buffer(message + "\r\n"));
 }
 
 void Irc::_readHandler(const boost::tokenizer<boost::char_separator<char> > &tokenizer)
 {
-	for(auto it : _readHandlers)
-		it(tokenizer);
+    for(auto it : _readHandlers)
+        it(tokenizer);
 }
 
 void Irc::_connectHandler(const boost::system::error_code &error)
 {
-	if(!error)
-	{
-		_onConnect();
-	}
+    if(!error)
+    {
+        _onConnect();
+    }
 }
 
 void Irc::_pong(const std::string &ping)
--- irc.hpp
+++ irc.hpp
@@ -4,42 +4,43 @@
 #include <vector>
 #include <boost/asio.hpp>
 #include <boost/tokenizer.hpp>
+#include <boost/shared_ptr.hpp>
 
-class Irc
+class Irc 
 {
-	public:
-		Irc(const std::string &server, const std::string &port, const std::function<void()> onConnect);
+    public:
+        Irc(const std::string &server, const std::string &port, const std::function<void()> onConnect);
 
-		void connect();
-		void close();
+        void connect();
+        void close();
 
-		void user(const std::string &username);
-		void user(const std::string &username, const std::string &hostname, const std::string &server, const std::string &realname);
-		void nick(std::string &nickname);
-		void join(const std::string &chan);
-		void part(const std::string &chan);
-		void privmsg(const std::string &to, const std::string &msg);
-		void command(const std::string &cmd, const std::string &msg);
-		void command(const std::string &cmd, const std::string &to, const std::string &msg);
+        void user(const std::string &username);
+        void user(const std::string &username, const std::string &hostname, const std::string &server, const std::string &realname);
+        void nick(std::string &nickname);
+        void join(const std::string &chan);
+        void part(const std::string &chan);
+        void privmsg(const std::string &to, const std::string &msg);
+        void command(const std::string &cmd, const std::string &msg);
+        void command(const std::string &cmd, const std::string &to, const std::string &msg);
 
-		void run();
+        void run();
 
-	private:
-		void _read(const boost::system::error_code &error);
-		void _send(std::string &message);
-		void _readHandler(const boost::tokenizer<boost::char_separator<char> > &tokenizer);
-		void _connectHandler(const boost::system::error_code &error);
+    private:
+        void _read(const boost::system::error_code &error);
+        void _send(std::string &message);
+        void _readHandler(const boost::tokenizer<boost::char_separator<char> > &tokenizer);
+        void _connectHandler(const boost::system::error_code &error);
 
-		void _pong(const std::string &ping);
-		
-		std::string _server;
-		std::string _port;
-		std::string _chan;
-		std::vector<std::function<void (const boost::tokenizer<boost::char_separator<char> >&)>> _readHandlers;
-		std::function<void()> _onConnect;
-		boost::asio::streambuf _buffer;
-		boost::asio::io_service _ios;
-		boost::asio::ip::tcp::socket _socket;
+        void _pong(const std::string &ping);
+
+        std::string _server;
+        std::string _port;
+        std::string _chan;
+        std::vector<std::function<void (const boost::tokenizer<boost::char_separator<char> >&)>> _readHandlers;
+        std::function<void()> _onConnect;
+        boost::asio::streambuf _buffer;
+        boost::asio::io_service _ios;
+        boost::shared_ptr<boost::asio::ip::tcp::socket> _socket;
 };
 
 #endif
